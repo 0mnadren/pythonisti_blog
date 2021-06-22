8d06714c0e7a53016804f08569b69e4ecc82d9ad
nemanja <neurit93@gmail.com>
2021-06-22 18:24:55 +0200

    added postgre url

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -20,7 +20,8 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ######## DATABASE SETUP #########
 #################################
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
