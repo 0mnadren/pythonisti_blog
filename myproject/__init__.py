47a392b06dcbc80ee9a7ef282369319b7b2f0950
nemanja <neurit93@gmail.com>
2021-06-23 16:21:15 +0200

    try with filter_by

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -19,11 +19,11 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ######## DATABASE SETUP #########
 #################################
 basedir = os.path.abspath(os.path.dirname(__file__))
-uri = os.environ.get("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# uri = os.environ.get("DATABASE_URL")  # or other relevant config var
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://", 1)
+# app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
